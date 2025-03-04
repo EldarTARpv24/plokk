b7f8c55f1a036f8d0ade12fd63d4911aec3dee0f
Eldar Aliev <eldar040503@gmail.com>
2025-03-04 09:35:18 +0200

    Add project files.

---
+++ plokk.py
@@ -0,0 +1,31 @@
+# v5 3 Rühm 20 õpilast sooritas ühe sessiooni jooksul kolm eksamit. Tehke algoritm eksamivormi täitmiseks.
+
+# for õ in range(20):
+#     print(f"Sooritaja {õ+1} õpilane")
+#     for e in range(3):
+#         print(f"{e+1} eksam")
+
+# v4 2
+
+# P = int(input("Mitu korda kordame?"))
+# vast = 0
+# while True:
+#     arv= float(input("Sisesta arv: "))
+#     if arv < 0:
+#         vast += arv
+#     P -= 1
+#     if P == 0:
+#             break
+# print("Vastus on", vast)
+
+# v1 4
+
+kokku = int(input("Скок? "))
+panni_maht = int(input("Скок на сковороде? ")) 
+aeg = 0 
+
+for a in range(0, kokku, panni_maht):
+    if a > 0:
+        kokku -= panni_maht
+        aeg += 1
+        print(f"{aeg} времени. {kokku} осталось")
